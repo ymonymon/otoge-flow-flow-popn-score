b6c14d5478555caaf62c7ea7544cd5188b19e268
ymonymon <ymonymon@gmail.com>
2022-05-15 14:29:06 +0900

    disable warning.

--- PopnScoreTool2/wwwroot/js/const.js
+++ PopnScoreTool2/wwwroot/js/const.js
@@ -1,4 +1,6 @@
-﻿/*
+﻿/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+/*
  Copyright (c) 2020-2022 otoge-flow-flow.com
  Released under the MIT license
  https://opensource.org/licenses/mit-license.php
