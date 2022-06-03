dc436f5bc708eaf5270d23f5fa6c1eaafdb1851f
ymonymon <ymonymon@gmail.com>
2022-06-03 22:55:48 +0900

    update: eslint - adopt airbnb-base

--- PopnScoreTool2/.eslintrc.js
+++ PopnScoreTool2/.eslintrc.js
@@ -1,23 +1,20 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    // 'eslint:recommended',
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+  },
 };
