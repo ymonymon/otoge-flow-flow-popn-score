ee7d721e294ef448a26f2b42a746a3e21b2669b9
ymonymon <ymonymon@gmail.com>
2023-04-15 20:08:10 +0900

    Grant execution permission of stored procedure to maintenance user to add/update fumen information.

--- Database/db-init.sql
+++ Database/db-init.sql
@@ -34,10 +34,16 @@ BEGIN
         CREATE DATABASE [PopnScoreTool2]
     END CATCH
 END;
-
+GO
 
 -- login userの作成
 USE [master]
+GO
+
+IF EXISTS (SELECT * FROM sys.server_principals WHERE name = 'PopnScoreTool2_Login' AND type = 'S')
+BEGIN
+    DROP LOGIN [PopnScoreTool2_Login]
+END
 GO
 
 CREATE LOGIN [PopnScoreTool2_Login] WITH PASSWORD=N'$(USER_PST2_PASSWORD)', DEFAULT_DATABASE=[PopnScoreTool2], DEFAULT_LANGUAGE=[日本語], CHECK_EXPIRATION=OFF, CHECK_POLICY=ON
@@ -49,7 +55,13 @@ GO
 USE [PopnScoreTool2]
 GO
 
-DROP USER IF EXISTS [PopnScoreTool2_User]
+IF EXISTS (SELECT * FROM sys.database_principals WHERE name = 'PopnScoreTool2_User')
+BEGIN
+    -- You can ignore the following error message. The user has been successfully deleted.
+    -- Currently, there is no known method to suppress this error message from appearing.
+    -- Error Message: Couldn't remove the user (n) from database (n) from the external libraries folder, error: 0x80070003
+    DROP USER [PopnScoreTool2_User];
+END
 GO
 
 CREATE USER [PopnScoreTool2_User] FOR LOGIN [PopnScoreTool2_Login] WITH DEFAULT_SCHEMA=[dbo]
@@ -58,4 +70,10 @@ GO
 EXEC sp_addrolemember 'db_datareader', 'PopnScoreTool2_User'
 GO
 EXEC sp_addrolemember 'db_datawriter', 'PopnScoreTool2_User'
+GO
+
+GRANT EXECUTE ON [dbo].[MergeMusic] TO [PopnScoreTool2_User]
+GO
+
+RAISERROR('End of db-init.sql', 0, 1) WITH NOWAIT;
 GO
