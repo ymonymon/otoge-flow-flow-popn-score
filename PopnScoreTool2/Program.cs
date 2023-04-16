88e27f3c67df6abf8b61ba76817f4ede7f7f50df
ymonymon <ymonymon@gmail.com>
2023-04-17 04:24:50 +0900

    Expression-bodied members

--- PopnScoreTool2/Program.cs
+++ PopnScoreTool2/Program.cs
@@ -31,11 +31,13 @@ namespace PopnScoreTool2
 
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+        }
     }
 }
