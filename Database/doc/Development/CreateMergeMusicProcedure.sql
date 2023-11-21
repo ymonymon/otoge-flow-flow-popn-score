2c8e3ec4efcd6eb30b06ee6f20817c1eb635bc97
ymonymon <ymonymon@gmail.com>
2023-11-22 03:38:23 +0900

    :heavy_plus_sign: Added MergeMusics2 procedure.

--- Database/doc/Development/CreateMergeMusicProcedure.sql
+++ Database/doc/Development/CreateMergeMusicProcedure.sql
@@ -101,3 +101,146 @@ BEGIN
         );
 END
 GO
+
+IF OBJECT_ID('dbo.MergeMusic2', 'P') IS NOT NULL
+    DROP PROCEDURE dbo.MergeMusic2;
+GO
+
+CREATE PROCEDURE [dbo].[MergeMusic2]
+    @Id int,
+    @MusicId int,
+    @Name nvarchar(max),
+    @Genre nvarchar(max),
+    @Position int,
+    @Level int,
+    @LevelId int,
+    @Version int,
+    @AddVersion int,
+    @GenreIndexCompareForPosition int,
+    @TitleIndexCompareForPosition int,
+    @Deleted bit,
+	@AddDate datetime2,
+	@DeletedDate datetime2,
+	@MinBPM int,
+	@MaxBPM int,
+	@DisplayedBPM nvarchar(max),
+	@DetailedBPM nvarchar(max),
+	@LPFlag bit,
+	@ArtistsName nvarchar(max)
+AS
+BEGIN
+    MERGE INTO [dbo].[Musics] AS target
+    USING (
+        VALUES (
+            @Id,
+            @MusicId,
+            @Name,
+            @Genre,
+            @Position,
+            @Level,
+            @LevelId,
+            @Version,
+            @AddVersion,
+            @GenreIndexCompareForPosition,
+            @TitleIndexCompareForPosition,
+            @Deleted,
+			@AddDate,
+			@DeletedDate,
+			@MinBPM,
+			@MaxBPM,
+			@DisplayedBPM,
+			@DetailedBPM,
+			@LPFlag,
+			@ArtistsName
+        )
+    ) AS source (
+        [Id],
+        [MusicId],
+        [Name],
+        [Genre],
+        [Position],
+        [Level],
+        [LevelId],
+        [Version],
+        [AddVersion],
+        [GenreIndexCompareForPosition],
+        [TitleIndexCompareForPosition],
+        [Deleted],
+		[AddDate],
+		[DeletedDate],
+		[MinBPM],
+		[MaxBPM],
+		[DisplayedBPM],
+		[DetailedBPM],
+		[LPFlag],
+		[ArtistsName]
+    )
+    ON target.[Id] = source.[Id]
+    WHEN MATCHED THEN
+        UPDATE SET
+            target.[MusicId] = source.[MusicId],
+            target.[Name] = source.[Name],
+            target.[Genre] = source.[Genre],
+            target.[Position] = source.[Position],
+            target.[Level] = source.[Level],
+            target.[LevelId] = source.[LevelId],
+            target.[Version] = source.[Version],
+            target.[AddVersion] = source.[AddVersion],
+            target.[GenreIndexCompareForPosition] = source.[GenreIndexCompareForPosition],
+            target.[TitleIndexCompareForPosition] = source.[TitleIndexCompareForPosition],
+            target.[Deleted] = source.[Deleted],
+			target.[AddDate] = source.[AddDate],
+			target.[DeletedDate] = source.[DeletedDate],
+			target.[MinBPM] = source.[MinBPM],
+			target.[MaxBPM] = source.[MaxBPM],
+			target.[DisplayedBPM] = source.[DisplayedBPM],
+			target.[DetailedBPM] = source.[DetailedBPM],
+			target.[LPFlag] = source.[LPFlag],
+			target.[ArtistsName] = source.[ArtistsName]
+    WHEN NOT MATCHED BY TARGET THEN
+        INSERT (
+            [Id],
+            [MusicId],
+            [Name],
+            [Genre],
+            [Position],
+            [Level],
+            [LevelId],
+            [Version],
+            [AddVersion],
+            [GenreIndexCompareForPosition],
+            [TitleIndexCompareForPosition],
+			[Deleted],
+			[AddDate],
+			[DeletedDate],
+			[MinBPM],
+			[MaxBPM],
+			[DisplayedBPM],
+			[DetailedBPM],
+			[LPFlag],
+			[ArtistsName]
+        )
+        VALUES (
+            source.[Id],
+            source.[MusicId],
+            source.[Name],
+            source.[Genre],
+            source.[Position],
+            source.[Level],
+            source.[LevelId],
+            source.[Version],
+            source.[AddVersion],
+            source.[GenreIndexCompareForPosition],
+            source.[TitleIndexCompareForPosition],
+            source.[Deleted],
+			source.[AddDate],
+			source.[DeletedDate],
+			source.[MinBPM],
+			source.[MaxBPM],
+			source.[DisplayedBPM],
+			source.[DetailedBPM],
+			source.[LPFlag],
+			source.[ArtistsName]
+        );
+END
+GO
--- Database/doc/Development/GrantMergeMusicProcedure.sql
+++ Database/doc/Development/GrantMergeMusicProcedure.sql
@@ -2,3 +2,5 @@ USE [PopnScoreTool2]
 GO
 GRANT EXECUTE ON [dbo].[MergeMusic] TO [PopnScoreTool2_User]
 GO
+GRANT EXECUTE ON [dbo].[MergeMusic2] TO [PopnScoreTool2_User]
+GO
